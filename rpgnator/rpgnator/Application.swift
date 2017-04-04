46dae93dae2580cda238c3d0a5405747cd830adf
cdsm <cdsm@macpf01.local>
2017-04-04 10:26:57 +0200

    Add application informations

---
+++ rpgnator/rpgnator/Application.swift
@@ -0,0 +1,14 @@
+/**
+ Used to store the project's central informations
+ - Author:
+    David Schnepp
+ - Version:
+    0.1.0
+*/
+
+import Foundation
+
+class Application {
+    static let version = "0.0.0 2017-04-04 10:01"
+    static let name = "Rpgnator"
+}
--- rpgnator/rpgnator/main.swift
+++ rpgnator/rpgnator/main.swift
@@ -1,19 +1,20 @@
-//
-//  main.swift
-//  rpgnator the requirements for this game are:
-// - 2 players are added
-// - each player can choose from 3 classes: Fighter, Archer, Wizard
-// - each class has it's own weapon with different damages
-// - the game is played in trurns, each turn each player choose it's action: Attack or Enhance
-// - if the player chose Attack, he removes his weapon's damage from the other player's health
-// - if the player chose Enhance, 5 damage points are added to his weapon
-// - the game end when one player's health reachs 0
-//
-//  Created by cdsm on 04/04/2017.
-//  Copyright © 2017 com.schneppd.lab. All rights reserved.
-//
+/**
+  A simple RPG in swift
+  rpgnator the requirements for this game are:
+ - Author:
+    David Schnepp
+ - Important:
+ The project requirements are:
+    - 2 players are added
+    - each player can choose from 3 classes: Fighter, Archer, Wizard
+    - each class has it's own weapon with different damages
+    - the game is played in trurns, each turn each player choose it's action: Attack or Enhance
+    - if the player chose Attack, he removes his weapon's damage from the other player's health
+    - if the player chose Enhance, 5 damage points are added to his weapon
+    - the game end when one player's health reachs 0
+*/
 
 import Foundation
 
-print("Hello, World!")
+print("Welcome to \(Application.name) version \(Application.version)")
 
