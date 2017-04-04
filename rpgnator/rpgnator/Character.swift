9d1ac4fc53d832fc9a593c0ae70989b55f84405b
cdsm <cdsm@macpf01.local>
2017-04-04 10:33:53 +0200

    Add Character logic

--- rpgnator/rpgnator/Character.swift
+++ rpgnator/rpgnator/Character.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+enum CharacterType: CShort {
+    case Archer = 1, Fighter = 2, Wizard = 3
+}
+
+class Character {
+    var name:String
+    var type:CharacterType
+    var weapon:Weapon
+    var life:Int = 100
+    var opponent:Character? = nil
+    var idPlayer:CShort
+    
+    init(cIdPlayer:CShort, cName:String, cType:CShort) {
+        idPlayer = cIdPlayer
+        name = cName
+        
+        type = CharacterType(rawValue: cType)!
+        switch(type){
+        case CharacterType.Archer:
+            weapon = Bow()
+        case CharacterType.Fighter:
+            weapon = Sword()
+        case CharacterType.Wizard:
+            weapon = Staff()
+        }
+    }
+    
+    func SetOppenent(otherCharacter:Character) {
+        opponent = otherCharacter
+    }
+    
+    func Attack() {
+        opponent!.life -= weapon.damage
+    }
+    
+    func ImproveWeapon() {
+        weapon.damage += 5
+    }
+    
+    func IsDead() -> Bool {
+        return !(life >= 0)
+    }
+}
