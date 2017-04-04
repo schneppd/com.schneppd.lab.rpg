f91ddfabe616700694ab8d0ca31ab3c43d54eac7
cdsm <cdsm@macpf01.local>
2017-04-04 09:44:45 +0200

    Start of the project v 0.0.0 2017-04-04

---
+++ rpgnator/rpgnator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  rpgnator
+//
+//  Created by cdsm on 04/04/2017.
+//  Copyright © 2017 com.schneppd.lab. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
